a6c2c7b72218059b82a61f79d901c69b740437fa
Eduardo Pedroso <eduardo.pedroso@matera.com>
2016-09-01 21:08:21 -0300

    another commit

--- www/js/app.js
+++ www/js/app.js
@@ -22,3 +22,25 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ionic.contrib.ui.tin
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // Each tab has its own nav history stack:
+
+  .state('home', {
+    url: '/home',
+    templateUrl: '../templates/home.html',
+    controller: 'HomeCtrl'
+  })
+
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/home');
+
+});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,3 +1,17 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('HomeCtrl', function($scope) {
+  $scope.cards = [
+    {name:"111111"},
+    {name:"222222"}
+  ];
+
+  $scope.cardDestroyed = function(index) {
+    $scope.cards.splice(index, 1);
+  };
+
+  $scope.cardSwiped = function(index) {
+    var newCard = // new card data
+    $scope.cards.push(newCard);
+  };
+})
